a9027278d9933704ce22da651ea9cb9abcc600ad
Douglas-Bonelli <doulas.bonelli@gmail.com>
2020-11-20 15:40:45 -0300

    Inclusao das Entities e Enuns

---
+++ src/main/java/br/com/intelligenceWeb/LojaEcommerce/api/entities/Funcionario.java
@@ -0,0 +1,252 @@
+package br.com.intelligenceWeb.LojaEcommerce.api.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import br.com.intelligenceWeb.LojaEcommerce.api.enums.PerfilEnum;
+
+@Entity
+@Table( name = "funcionario" )
+public class Funcionario implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	private String email;
+	private String senha;
+	private String cpf;
+	private BigDecimal valorHora;
+	private Float qtdHorasTrabalho;
+	private Float qtdHorasAlmoco;
+	private PerfilEnum perfil;
+	private Date dataCriacao;
+	private Date dataAtualizacao;
+	private Empresa empresa;
+	private List<Lancamento> lancamentos;
+	
+	
+	public Funcionario() {
+	}
+
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	@Column(name = "ds_nome" , nullable = false )
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Column(name = "ds_email" , nullable = false )
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+    
+	@Column(name = "ds_senha" , nullable = false )
+	public String getSenha() {
+		return senha;
+	}
+
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	
+	@Column(name = "ds_cpf" , nullable = false )
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+	
+	@Column(name = "vl_hora" , nullable = true )
+	public BigDecimal getValorHora() {
+		return valorHora;
+	}
+
+
+	public void setValorHora(BigDecimal valorHora) {
+		this.valorHora = valorHora;
+	}
+	
+	@Transient
+	public Optional<BigDecimal> getValorHoraOpt(){
+	    return Optional.ofNullable(valorHora);
+	}
+	
+	
+	
+	
+
+	@Column(name = "qt_horas_trabalho" , nullable = true )
+	public Float getQtdHorasTrabalho() {
+		return qtdHorasTrabalho;
+	}
+
+
+	public void setQtdHorasTrabalho(Float qtdHorasTrabalho) {
+		this.qtdHorasTrabalho = qtdHorasTrabalho;
+	}
+	
+	
+	@Transient
+	public Optional<Float> getQtdHorasTrabalhoOpt(){
+	    return Optional.ofNullable(qtdHorasTrabalho);
+	}
+
+
+	
+	@Column(name = "qt_horas_almoco" , nullable = true )
+	public Float getQtdHorasAlmoco() {
+		return qtdHorasAlmoco;
+	}
+
+
+	public void setQtdHorasAlmoco(Float qtdHorasAlmoco) {
+		this.qtdHorasAlmoco = qtdHorasAlmoco;
+	}
+	
+	
+	@Transient
+	public Optional<Float> getQtdHorasAlmocoOpt(){
+	    return Optional.ofNullable(qtdHorasAlmoco);
+	}
+
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "ds_perfil" , nullable = false )
+	public PerfilEnum getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(PerfilEnum perfil) {
+		this.perfil = perfil;
+	}
+
+
+	@Column(name = "dt_criacao" , nullable = true )
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+
+	@Column(name = "dt_atualizacao" , nullable = true )
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+
+	@ManyToOne( fetch = FetchType.EAGER )
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+
+	@OneToMany(mappedBy = "funcionario" , fetch = FetchType.LAZY , cascade = CascadeType.ALL )
+	public List<Lancamento> getLancamentos() {
+		return lancamentos;
+	}
+
+
+	public void setLancamentos(List<Lancamento> lancamentos) {
+		this.lancamentos = lancamentos;
+	}
+
+	
+	@PreUpdate
+	public void preUpdate(){
+	   this.dataAtualizacao = new Date();
+	}
+
+	
+	
+	@PrePersist
+	public void prePersist(){
+	  final Date atual = new Date();
+	  dataCriacao = atual;
+	  dataAtualizacao = atual;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Funcionario [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + ", cpf=" + cpf
+				+ ", valorHora=" + valorHora + ", qtdHorasTrabalho=" + qtdHorasTrabalho + ", qtdHorasAlmoco="
+				+ qtdHorasAlmoco + ", perfil=" + perfil + ", dataCriacao=" + dataCriacao + ", dataAtualizacao="
+				+ dataAtualizacao + ", empresa=" + empresa + ", lancamentos=" + lancamentos + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/br/com/intelligenceWeb/LojaEcommerce/api/enums/PerfilEnum.java
@@ -0,0 +1,8 @@
+package br.com.intelligenceWeb.LojaEcommerce.api.enums;
+
+public enum PerfilEnum {
+	
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+
+}
